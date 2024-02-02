5223c9d0c79b1222434c9ce4e2dee55a94f6e6ab
Neil Shukla <neilpshukla221203@gmail.com>
2024-02-02 21:12:01 +0530

    temporary middleware fix

--- middleware.ts
+++ middleware.ts
@@ -4,8 +4,8 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
 export default authMiddleware({
-  afterAuth : (auth) => console.log(auth),
-  publicRoutes: ["/api/webhook"]
+  // afterAuth : (auth) => console.log(auth),
+  publicRoutes: ["/api/webhook", "/sign-up", "/sign-in"]
 });
  
 export const config = {
